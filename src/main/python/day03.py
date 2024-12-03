fe3ff12d8a103fff0552ae3be3d4ddef8b2d8815
Chris Britton <chris.m.britton@gmail.com>
2024-12-02 23:47:00 -0600

    Checkpoint

--- src/main/python/day03.py
+++ src/main/python/day03.py
@@ -9,6 +9,8 @@ from typing import Any
 
 from src.main.python.util import AbstractSolver
 
+PATTERN = r"(mul)\((\d{1,3}),(\d{1,3})\)|(do\(\))|(don't\(\))"
+
 DAY = '03'
 
 
@@ -16,23 +18,22 @@ class Solver(AbstractSolver):
     def __init__(self) -> None:
         super().__init__()
 
-    @staticmethod
-    def _init_data(data: list[str]) -> Any:
-        return [[int(x) for x in line.split(' ')] for line in data]
-
     def solve_part_1(self, data: Any, **kwargs) -> int:
         total = 0
         for line in data:
-            m = re.findall(r'mul\((\d{1,3}),(\d{1,3})\)', line)
-            for x in m:
-                total += int(x[0]) * int(x[1])
+            m = re.findall(PATTERN, line)
+            total += sum(int(x[1]) * int(x[2]) for x in m if x[0] == 'mul')
+            # for x in m:
+            #     if x[0] == 'mul':
+            #         total += int(x[1]) * int(x[2])
+
         return total
 
     def solve_part_2(self, data: Any, **kwargs) -> int:
         total = 0
         enabled = True
         for line in data:
-            m = re.findall(r"(mul)\((\d{1,3}),(\d{1,3})\)|(do\(\))|(don't\(\))", line)
+            m = re.findall(PATTERN, line)
             for x in m:
                 if x[3] == 'do()':
                     enabled = True
@@ -40,6 +41,7 @@ class Solver(AbstractSolver):
                     enabled = False
                 elif enabled:
                     total += int(x[1]) * int(x[2])
+
         return total
 
     # 70553436 is wrong
