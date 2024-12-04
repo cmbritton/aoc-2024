189e57563d2f17f14c8120363d6a46323bbd6d34
Chris Britton <chris.m.britton@gmail.com>
2024-12-04 01:49:09 -0600

    Checkpoint
    diagonals_ur_to_ll works

--- src/main/python/day04.py
+++ src/main/python/day04.py
@@ -6,8 +6,6 @@ https://adventofcode.com/2024/day/4
 """
 from typing import Any
 
-from PIL.ImageChops import offset
-
 from src.main.python.util import AbstractSolver
 
 DAY = '04'
@@ -63,10 +61,34 @@ class Solver(AbstractSolver):
 
         return new_rows
 
+    @staticmethod
+    def diagonals_ur_to_ll(matrix: list[str]) -> list[str]:
+        n = len(matrix)
+        new_rows = []
+
+        for col in range(n - 1, -1, -1):
+            new_row = []
+            offset = 0
+            for row in range(col + 1):
+                new_row.append(matrix[row][col - offset])
+                offset += 1
+            new_rows.append(''.join(new_row))
+
+        for row in range(1, n):
+            new_row = []
+            offset = 0
+            for col in range(n - 1, row - 1, -1):
+                new_row.append(matrix[row + offset][col])
+                offset += 1
+            new_rows.append(''.join(new_row))
+
+        return new_rows
+
     def solve_part_1(self, data: Any, **kwargs) -> int:
         reversed_data = data[::-1]
         ul_to_lr_data = Solver.diagonals_ul_to_lr(data)
         ll_to_ur_data = Solver.diagonals_ll_to_ur(data)
+        ur_to_ll_data = Solver.diagonals_ur_to_ll(data)
         # ll_to_ur_data = ''.join(list(map(list, zip(*data))))
         # ur_to_ll_data = ''.join(list(map(list, zip(*reversed_data)))[::-1])
         # lr_to_ul_data = ''.join(list(map(list, zip(*data)))[::-1])
