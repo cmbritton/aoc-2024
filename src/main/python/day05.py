abab9636fab21682516529ab4011ede3ece3329a
Chris Britton <chris.m.britton@gmail.com>
2024-12-04 04:00:19 -0600

    Prepare day 5

---
+++ src/main/python/day05.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+"""
+Day 5: TBD
+
+https://adventofcode.com/2024/day/5
+"""
+import re
+from typing import Any
+
+from src.main.python.util import AbstractSolver
+
+DAY = '05'
+
+
+class Solver(AbstractSolver):
+    def __init__(self) -> None:
+        super().__init__()
+
+
+    def solve_part_1(self, data: Any, **kwargs) -> int:
+        return 0
+
+    def solve_part_2(self, data: Any, **kwargs) -> int:
+        return 0
+
+    def get_day(self):
+        return DAY
+
+
+def main() -> None:
+    solver = Solver()
+    solver.run()
+
+
+if __name__ == "__main__":
+    main()
